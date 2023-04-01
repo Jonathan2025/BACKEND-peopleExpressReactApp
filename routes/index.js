b872e6ab35c100addcd6f9ffdbbb06ab25c8f8c5
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-01 18:37:12 -0400

    10 setting up the /routes/index.js, suppose to be set up earlier in the app

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
+// step 10 - where we will set up route and what our indexes will be for each route 
+const router = require('express').Router()
+const peopleRoute = require('./peopleRoutes') // import people routes methods 
 
+// URL directory 
+router.use('/people', peopleRoute)
+
+
+module.exports = router 
